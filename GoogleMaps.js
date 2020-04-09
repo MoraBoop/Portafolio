f873d3302b85bc57d56f7ef94edde6cbfe680702
Jason <jasonmoramora@outlook.com>
2020-04-09 02:05:53 -0500

    Listo

--- GoogleMaps.js
+++ GoogleMaps.js
@@ -1,8 +1,17 @@
-var map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 11,
-        mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain']
+    let coordenada = {lat: -12.065957, lng: -77.037077};
+    let map = new google.maps.Map(document.getElementById('map'), {
+        center: coordenada,
+        zoom: 15,
+        mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain']        
     });
+
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({
+        position: coordenada, 
+        map: map,
+        animation:google.maps.Animation.BOUNCE
+    });
+
+    
 }
