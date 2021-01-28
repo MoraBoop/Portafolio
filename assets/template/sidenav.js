2774fd8e9cecb11e61afab155e87d4e2e9ecc660
jason <jasonjairmoramora@gmail.com>
2021-01-28 00:43:44 -0500

    ok2

---
+++ assets/template/sidenav.js
@@ -0,0 +1,12 @@
+const menuIcon = document.querySelector('.menu-icon')
+
+menuIcon.addEventListener('click', ()=>{
+  const menu = document.querySelector('.sidenav')
+  console.log(menu)
+  // menu.classList.toggle('menu-active')
+  if (menu.classList.contains('menu-active')) {
+
+  }
+})
+
+
